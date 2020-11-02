8c48d3b62dad35494a673338117e74aff2b10a4a
WallaceGomes <wallacecardosogomes@gmail.com>
2020-11-02 13:44:59 -0300

    chore: initial backend setup and frontend connection

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const { request } = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const express = require('express');
+const products = require('./data/products');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('Api running');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+
+	res.json(product);
+});
+
+app.listen(
+	process.env.PORT || 5000,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${
+			process.env.PORT || 5000
+		}`,
+	),
+);
