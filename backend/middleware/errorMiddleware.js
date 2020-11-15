0260fca809cb24e8353d6eff317518103d4d1e39
WallaceGomes <wallacecardosogomes@gmail.com>
2020-11-14 23:48:31 -0300

    chore: adding a general error midleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+
+	res.status(error.code || 500);
+	res.json({
+		message: error.message || 'An unknown error occurred! (api routes) ',
+	});
+};
+
+module.exports = errorHandler;
